8825ee2c507ee9a884f4f343b96ef711e1be287c
lucascardone <lucas.a.cardone@gmail.com>
2025-05-10 19:40:20 -0300

    sufro

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -4,16 +4,15 @@ import Logo from "../componentes/svgs/Logo";
 import OlasFooter from "../componentes/svgs/OlasFooter";
 
 import { palette } from "../styles/constants";
-
 function HomeScreen() {
     const navigate = useNavigate();
 
     return (
-        <div className="d-flex flex-column min-vh-100 bg-crema">
-            <div className="top">
+        <div className="home-layout bg-crema text-center">
+            <div className="header">
                 <OlasHead color={palette.verde} />
             </div>
-            <div className="flex-grow-1 d-flex flex-column justify-content-center align-items-center text-center px-4">
+            <div className="main d-flex flex-column justify-content-center align-items-center px-4">
                 <div className="mb-4">
                     <Logo />
                 </div>
@@ -29,19 +28,24 @@ function HomeScreen() {
                 </button>
                 <p className="small">
                     ¿Tenés un carrito?{" "}
-                    <a href="#" className="text-rojo" onClick={(e) => {
-                        e.preventDefault();
-                        navigate("/registrar");
-                    }}>
+                    <a
+                        href="#"
+                        className="text-rojo"
+                        onClick={(e) => {
+                            e.preventDefault();
+                            navigate("/registrar");
+                        }}
+                    >
                         Registralo
                     </a>
                 </p>
             </div>
-            <div className="bottom">
+            <div className="footer">
                 <OlasFooter color={palette.verde} />
             </div>
         </div>
     );
 }
+
 
 export default HomeScreen;
